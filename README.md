dc1b09defe7d69d07861ed2f3f2c9020c0a04e4a
artyomturkoff <art.turkoff@gmail.com>
2026-05-09 22:06:44 +0200

    setting up new project

---
+++ README.md
@@ -0,0 +1,1 @@
+"IU DLBAIPNLP01 – NLP Project - Sentiment Analysis on Movie Reviews"
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from imdb-sentiment-analysis!")
+
+
+if __name__ == "__main__":
+    main()
